e7f89310c37caf9d2c6434e095b115005b4fff4d
eklochkova101 <260723543+eklochkova101@users.noreply.github.com>
2026-05-10 21:37:56 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,54 +5,55 @@
 template<typename T>
 class TPQueue {
  private:
-   static int getPrior(const T& item) {
-     return item.prior;
-   }
-   void listCleaner() {
-     Node* currn = root;
-     while (currn != nullptr) {
-       Node* next = currn->next;
-       delete currn;
-       currn = next;
-     }
-     root = nullptr;
-   }
-   struct Node {
-     T value;
-     Node* next;
-     Node(const T& item) : value(item), next(nullptr) {}
-   };
-   Node* root;
+    static int getPrior(const T& item) {
+      return item.prior;
+    }
+    void listCleaner() {
+      Node* currn = root;
+      while (currn != nullptr) {
+        Node* next = currn->next;
+        delete currn;
+        currn = next;
+      }
+      root = nullptr;
+    }
+    struct Node {
+      T value;
+      Node* next;
+      explicit Node(const T& item) : value(item), next(nullptr) {}
+    };
+    Node* root;
+
  public:
-   TPQueue() : root(nullptr) {}
-   ~TPQueue() {
-     listCleaner();
-   }
-   void push(const T& item) {
-     Node* newbie = new Node(item);
-     if (root == nullptr || getPrior(item) > getPrior(root->value)) {
-       newbie->next = root;
-       root = newbie;
-       return;
-     }
-     Node* curr = root;
-     while (curr->next != nullptr &&
-       getPrior(curr->next->value) > getPrior(item)) {
-       curr = curr->next;
-     }
-     newbie->next = curr->next;
-     curr->next = newbie;
-   }
-   T pop() {
-     if (root == nullptr) {
-       throw "queue is empty(";
-     }
-     Node* oldRoot = root;
-     T answer = root->value;
-     root = root->next;
-     delete oldRoot;
-     return answer;
-   }
+    TPQueue() : root(nullptr) {}
+    ~TPQueue() {
+      listCleaner();
+    }
+    void push(const T& item) {
+      Node* newbie = new Node(item);
+      if (root == nullptr || getPrior(item) > getPrior(root->value)) {
+        newbie->next = root;
+        root = newbie;
+        return;
+      }
+      Node* curr = root;
+      while (curr->next != nullptr &&
+        getPrior(curr->next->value) > getPrior(item)) {
+        curr = curr->next;
+      }
+      newbie->next = curr->next;
+      curr->next = newbie;
+    }
+    T pop() {
+      if (root == nullptr) {
+        throw "queue is empty(";
+      }
+      Node* oldRoot = root;
+      T answer = root->value;
+      root = root->next;
+      delete oldRoot;
+      return answer;
+    }
 };
 struct SYM {
   char ch;
